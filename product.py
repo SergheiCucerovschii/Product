046e95d552825e565f2f11187e0885165d8b4f78
SergheiCucerovschii <74251573+SergheiCucerovschii@users.noreply.github.com>
2020-11-22 17:06:34 +0200

    Add files via upload

---
+++ product.py
@@ -0,0 +1,26 @@
+class Product:
+
+    def __init__(self,name,price,rating):
+        self.name = name
+        self.price = price
+        self.rating = rating
+
+    def __str__(self):
+        return f"-= PRODUCT ('{self.name:^15}' {self.price:}$ {self.rating}) =-"
+
+    def __gt__(self,w):
+        return self.rating > w.rating
+    def __lt__(self,w):
+        return self.rating < w.rating
+
+
+p1 = Product("Programming book", 100, 4.5)
+p2 = Product("Hacking book" ,  150, 5.0)
+
+print(p1)
+print(p2)
+
+if p1 > p2:
+    print("First book is better")
+elif p1 < p2:
+    print("Second book is better")
